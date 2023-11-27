1bb8ed98601d1d1b6fed9693cfe2e1bfe0639c2c
Fjodor <fjodorj94@gmail.com>
2023-11-27 19:28:07 +0200

    Add Constants class

---
+++ src/main/java/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    
+    // Making fields access modifiers PUBLIC allows for other classes to access them
+    public static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    public static final String NUMBERS = "1234567890";
+    public static final String SYMBOLS = " !@#$%^&*_=+-/.?<>)";
+    
+}
