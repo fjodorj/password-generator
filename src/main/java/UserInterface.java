3812e2ecfca542e4fea7f1f691a9bb0f26904372
Fjodor <fjodorj94@gmail.com>
2023-11-27 20:28:20 +0200

    Styling

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 public class UserInterface {
     
     public UserInterface() {}
-    
-    
-    
     public void start() {
         while (true) {
+            
             Scanner scanner = new Scanner(System.in);
             Generator generator = new Generator();
             
             System.out.print("Enter the length of your new password: ");
             Integer userInput = Integer.valueOf(scanner.nextLine());
             scanner.close();
+            
             generator.generate(userInput);
             System.out.println(generator.generate(userInput));
             break;
